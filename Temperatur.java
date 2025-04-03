ef49c18b2b3103f1725c3a4e2419bfdcd6c26715
PhilippS4lmen <150135142+PhilippS4lmen@users.noreply.github.com>
2025-04-03 14:06:20 +0000

    test
    Co-authored-by: Jolandarrr <Jolandarrr@users.noreply.github.com>

--- Temperatur.java
+++ Temperatur.java
@@ -2,7 +2,7 @@ public class Temperatur {
 
 public static void main(String[] args) {
 System.out.println("HelloWorl");
-System.out.println("helloworld");    
+System.out.println("helloworld");    //hallo
 }
 
 
